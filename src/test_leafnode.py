8d22f6aa855fdb29c9a15bc8838ee4e19d152b9f
Abdulaziz Zazai <zazai@Abdulazizs-MacBook-Air.local>
2026-06-10 03:12:34 +0100

    test: write tests for LeafNode

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -1,2 +1,11 @@
 import unittest
-from leaf
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_repr(self):
+        node = LeafNode("a", "Cool spot", {'key' : 'val'})
+        self.assertEqual(node.__repr__(), "LeafNode(a, Cool spot, {'key': 'val'})")
