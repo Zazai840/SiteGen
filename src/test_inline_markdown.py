20de95deba3a5967399fcd49b52e85ad3bd9f2d0
zazai <zazenquiries@gmail.com>
2026-07-07 14:33:42 +0100

    create markdown node delimeter function and write tests'

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,2 +1,31 @@
 import unittest
-from textnode import 
+from textnode import TextNode, TextType
+from inline_markdown import split_nodes_delimiter
+
+class test_split_nodes_delimiter(unittest.TestCase):
+    def test_code_text(self):
+        test_node = TextNode("this is a `code block` that is being tested", TextType.TEXT)
+        res = split_nodes_delimiter([test_node], "`", TextType.CODE)
+        expected_res = [TextNode("this is a ", TextType.TEXT), TextNode("code block", TextType.CODE), TextNode(" that is being tested", TextType.TEXT)]
+        self.assertEqual(res, expected_res)
+    
+    def test_bold_text(self):
+        test_node = TextNode("this is **bold text** that is being tested", TextType.TEXT)
+        res = split_nodes_delimiter([test_node], "**", TextType.BOLD)
+        expected_res = [TextNode("this is ", TextType.TEXT), TextNode("bold text", TextType.BOLD), TextNode(" that is being tested", TextType.TEXT)]
+        self.assertEqual(res, expected_res)
+    
+    def test_italic_text(self):
+        test_node = TextNode("this is _italic text_ that is being tested", TextType.TEXT)
+        res = split_nodes_delimiter([test_node], "_", TextType.ITALIC)
+        expected_res = [TextNode("this is ", TextType.TEXT), TextNode("italic text", TextType.ITALIC), TextNode(" that is being tested", TextType.TEXT)]
+        self.assertEqual(res, expected_res)
+
+    def test_delimiter_at_beginning_of_text(self):
+        test_node = test_node = TextNode("_italic text_ that is being tested", TextType.TEXT)
+        res = split_nodes_delimiter([test_node], "_", TextType.ITALIC)
+        expected_res = [TextNode("italic text", TextType.ITALIC),
+                        TextNode(" that is being tested", TextType.TEXT)]
+        self.assertEqual(res, expected_res)
+
+   
